586213f068122124dd34a39b21c91ac2f7c6769d
leyarotheconquerer <leyarotheconquerer@yahoo.com>
2021-10-01 20:15:27 -0500

    Add initial unreal project

---
+++ Ruin/Source/Ruin/RuinGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RuinGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RUIN_API ARuinGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
